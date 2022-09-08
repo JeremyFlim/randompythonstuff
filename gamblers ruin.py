d7c04dceddc8f7c8e86a3c39bb3819133c24294b
JeremyFlim <99420039+JeremyFlim@users.noreply.github.com>
2022-09-08 10:01:40 +0700

    added some features

--- gamblers ruin.py
+++ gamblers ruin.py
@@ -10,9 +10,9 @@ def game():
 
     goal = int(input("Input the goal... "))
 
-    games = int(input("Input the number of simulations to run... "))
-
     winChance = int(input("Input a win chance out of 100... "))
+
+    games = int(input("Input the number of simulations to run... "))
 
     winChance = winChance/100
 
@@ -22,35 +22,38 @@ def game():
     steps = 0
 
     coinFlip = False
-
     while gamesDone < games:
         stake = money
         while stake > 0 and stake < goal:
             seed(random())
             if random() < winChance:
                 stake = stake + 1
+                steps+=1
             else:
                 stake = stake - 1
+                steps+=1
         if stake >= goal:
             timesWon += 1
         gamesDone+=1
         print("Game " + str(gamesDone) + " finished...")
 
-    print("You won %s times and lost %s times!" % (timesWon,games - timesWon))
+    print("\nYou won %s times and lost %s times!" % (timesWon,games - timesWon))
 
     percentageWin = (timesWon/gamesDone)*100
+    stepsPerGame = (steps/games)
 
-    print("Your game had a percentage winrate of %s!" % percentageWin)
+    print("Your game had a percentage winrate of %s!\n" % percentageWin)
+    print("Each game took an average of %s steps to finish!\n" % stepsPerGame)
     retry()
 
 def retry():
     print("Would you like to run more simulations?\nType Y to continue, type anything else to exit...")
     retryChoice = input()
     if retryChoice.lower() == "y":
+        print("\n")
         game()
     else:
         exit()
 
 if __run__ == True:
     game()
-
